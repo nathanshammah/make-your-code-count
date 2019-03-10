6f94659566fbfd3e9721b9916299e95dcda763d2
quantshah <shahnawaz.ahmed95@gmail.com>
2019-03-10 14:16:54 +0100

    Added a mylibrary

---
+++ mylibrary/README.md
@@ -0,0 +1,4 @@
+# MyLibrary: Hi, this is a library to make your code count.
+
+This simply library prints a hello world message from a python package and
+tests its functionality.
---
+++ mylibrary/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mylibrary',
+      version='0.1',
+      description='Test',
+      author='Shahnawaz Ahmed, Nathan Shammah',
+      packages = ['mylibrary']
+     )
